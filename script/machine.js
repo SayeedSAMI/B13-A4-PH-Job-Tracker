5005d6db9c28d66efdaf3e8c702546ca42cae9b7
SayeedSami <sayeedsami26@gmail.com>
2026-02-27 12:53:49 +0600

    Feat:interview section added with interactive js

--- script/machine.js
+++ script/machine.js
@@ -1,6 +1,5 @@
 // show div after toggle button clicked
 
-
 function showinfo(id) {
   const all = document.getElementById("all");
   const interview = document.getElementById("interview");
@@ -14,3 +13,21 @@ function showinfo(id) {
   clicked.classList.remove("hidden");
 }
 
+function addToInterview(id) {
+  const removeEmpty = document.getElementById("interview-empty-section");
+  removeEmpty.classList.add("hidden");
+
+  const jobNo1 = document.getElementById(id);
+
+  const value = jobNo1.innerHTML;
+  const newdiv = document.createElement("div");
+
+  newdiv.innerHTML = `
+      <div class="border border-[#F1F2F4] p-5 rounded-lg space-y-3 bg-white"> 
+    ${value}
+    </div>
+    `;
+
+  const interviewSection = document.getElementById("interview");
+  interviewSection.append(newdiv);
+}
--- script/show.js
+++ script/show.js
@@ -2,20 +2,24 @@
 
 // interview section
 
-document
-  .getElementById("add-to-interview")
-  .addEventListener("click", function () {
-    const jobNo1 = document.getElementById("job-1");
+// document
+//   .getElementById("add-to-interview")
+//   .addEventListener("click", function () {
+//     addToInterview("job-1");
+//     // const removeEmpty = document.getElementById("interview-empty-section");
+//     // removeEmpty.classList.add("hidden");
 
-    const value = jobNo1.innerHTML;
-    const newdiv = document.createElement("div");
+//     // const jobNo1 = document.getElementById("job-1");
 
-    newdiv.innerHTML = `
-    <div id="all" class="space-y-4">
-    ${value}
-    </div>
-    `;
+//     // const value = jobNo1.innerHTML;
+//     // const newdiv = document.createElement("div");
 
-    const interviewSection = document.getElementById("interview");
-    interviewSection.append(newdiv);
-  });
+//     // newdiv.innerHTML = `
+//     //   <div class="border border-[#F1F2F4] p-5 rounded-lg space-y-3 bg-white">
+//     // ${value}
+//     // </div>
+//     // `;
+
+//     // const interviewSection = document.getElementById("interview");
+//     // interviewSection.append(newdiv);
+//   });
